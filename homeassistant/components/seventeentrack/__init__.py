400598fa48e9c8d319b5ee68c5ed11b574791c55
Aaron Bach <bachya1208@gmail.com>
2018-05-30 13:25:06 -0600

    Small comment update

--- homeassistant/components/seventeentrack/__init__.py
+++ homeassistant/components/seventeentrack/__init__.py
@@ -1,5 +1,5 @@
 """
-Support for package info from 17track.net
+Support for package info from 17track.net.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.seventeentrack/
